4f3c3dbcd2950222240bc50eaa8fa496d727222b
Özer Altun <ozerutkualtun@gmail.com>
2022-04-13 10:45:40 +0300

    _app.js and layout wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import Layout from "../components/layout/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Layout><Component {...pageProps} /></Layout>
 }
+
+// burası root component giriş yerimiz index dosyası olsa da main navigation'ın
+// her yerde görünmesi için Layout ile burayı wraplemeliyiz.
 
 export default MyApp
