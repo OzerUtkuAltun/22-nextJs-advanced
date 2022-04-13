f8c96bb9a06313051a0d0c327c50f87bbf3214ae
Özer Altun <ozerutkualtun@gmail.com>
2022-04-13 11:12:57 +0300

    custom components and css modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return <MeetupDetail image={"https://www.targettraining.eu/wp-content/uploads/2019/02/meetings-practice.jpg"}
+  alt = {"A first meetup"}
+  title= {"Title"}
+  address = {"Address"}
+  description = {"description"}
+  />  
+}
+
+export default MeetupDetails;
