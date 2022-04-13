2af8e2795b6929af9dbfb63d8c8a6392e61ea4d8
Özer Altun <ozerutkualtun@gmail.com>
2022-04-13 10:52:38 +0300

    using programmatic navigation (useRouter)

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,14 @@
-import Card from '../ui/Card';
-import classes from './MeetupItem.module.css';
+import Card from "../ui/Card";
+import classes from "./MeetupItem.module.css";
+import { useRouter } from "next/router";
 
 function MeetupItem(props) {
+
+  const router = useRouter();
+  function showDetailsHandler() {
+    router.push("/" + props.id)
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +20,8 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Show Details</button>
+          {/** normalde burada da link (next/link) kullanmak daha mantıklı olurdu. */}
         </div>
       </Card>
     </li>
