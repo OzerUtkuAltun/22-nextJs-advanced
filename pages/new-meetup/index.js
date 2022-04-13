a1ceb8d787fb182e6099a620ebcc126c79105c9c
Özer Altun <ozerutkualtun@gmail.com>
2022-04-13 10:37:08 +0300

    new meetup form

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,2 +1,15 @@
 // domain.com/new-meetup -> burada nested pathler için subfolder approach'ı seçtik.
 
+import React from "react";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData);
+    }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler}/>;
+}
+
+export default NewMeetupPage;
